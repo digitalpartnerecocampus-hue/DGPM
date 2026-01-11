9cccf441a0857982cd5d9748ea95ae1da280b31a
ECODGPM <digitalpartnerecocampus@gmail.com>
2026-01-11 17:56:02 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+// --- CONFIGURATION ---
+const CONFIG = {
+    // 1. SUPABASE (Database)
+    supabaseUrl: "https://rjzaikcjuycmecxgifzg.supabase.co",
+    supabaseKey: "sb_publishable_gdLqam4Fu7BPGBwBnA5cuQ_C2RPhzgq",
+
+
+    // 2. CLOUDINARY (Image Upload)
+    cloudinaryCloudName: "da1phbsy0",
+    cloudinaryUploadPreset: "Medias" // Create an "Unsigned" preset in Cloudinary settings
+};
